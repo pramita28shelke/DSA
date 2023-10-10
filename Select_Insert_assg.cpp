52d184da5147b8f749e1100a024687c09d55fcfe
Pramita Shelke <113428034+pramita28shelke@users.noreply.github.com>
2023-10-10 22:33:33 +0530

    Update Select_Insert_assg.cpp

--- Select_Insert_assg.cpp
+++ Select_Insert_assg.cpp
@@ -6,7 +6,7 @@
 // c) We swap the index of the minimum element with the first element of the array.
 // d) It takes O(n^2) swaps.
 
-// Solution : All are true
+// Solution : b]
 
 // Which of the following examples represent the worst case input for an insertion sort?
 // a) array in sorted order
@@ -14,7 +14,7 @@
 // c) normal unsorted array
 // d) array sorted in reverse order
 
-// Solution : Large array and array sorted in reverse order
+// Solution : d] array sorted in reverse order
 
 
 // How many passes would be required during insertion sort to sort an array of 5 elements?
@@ -52,6 +52,40 @@ int main(){
 // Given an array of digits (values are from 0 to 9), the task is to find the minimum possible sum of
 // two numbers formed from digits of the array. Please note that all digits of the given array must be
 // used to form the two numbers.
+    int arr[]={5,1,2,3,4};
+    int s=5;
+    for(int i=0;i<s;i++){
+    int j=i;
+    while(j>=1 && arr[j]<arr[j-1]){
+        swap(arr[j],arr[j-1]);
+        j--;
+    }
+ }
+// sorting tha array into an integer
+int x=0;
+for(int i=0;i<s;i++){
+    x = x*10;
+    x=x+arr[i];
+
+}
+cout<<x<<endl;
+
+//second minimum array 
+for(int i=s-1;i>=1;i--){
+    if(arr[i]!=arr[i-1]){
+        swap(arr[i],arr[i-1]);
+        break;
+    }
+}
+int y=0;
+for(int i=0;i<s;i++){
+    y= y*10;
+    y=y+arr[i];
+
+}
+cout<<y<<endl;
+cout<<"Minimum Sum : "<<x+y<<endl;
+
 
     return 0 ;
 }
